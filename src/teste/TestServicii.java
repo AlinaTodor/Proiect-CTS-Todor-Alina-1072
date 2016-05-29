747be0f67fd6ec35e95ba1a24b3c460931620ff9
AlinaTodor <alinamanuelatodor@gmail.com>
2016-05-29 23:48:23 +0300

    modificare test servicii

--- src/teste/TestServicii.java
+++ src/teste/TestServicii.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import clase.*;
+import clase.Serviciu.serviciu;
 
 public class TestServicii {
 	
@@ -24,8 +25,19 @@ public class TestServicii {
 	@Test
 	public void test() {
 		String sep=System.getProperty("line.separator");
-		Consultanta consultanta=new Consultanta();
-		//consultanta.set
+		IMM MGT=new IMM("MGT", "Bucuresti", serviciu.Consultanta);
+		Factory serviciuFactory=new Factory();
+		Consultanta consultanta=(Consultanta) serviciuFactory.getServiciu(serviciu.Consultanta, 1000, MGT, 12);
+		assertEquals(MGT.getNume(), consultanta.getClient().getNume());
+		assertEquals(12L, (long)consultanta.getDurata());
+		assertEquals(1000L, (long)consultanta.getPret());
+		consultanta.serviciu();
+		String expected="IMM-ul "+MGT.toString()+" solicita un nou serviciu "+serviciu.Consultanta.toString();
+		expected+=sep;
+		expected+="IMM-ul "+MGT.toString()+" a acceptat serviciul "+serviciu.Consultanta.toString();
+		expected+=sep;
+		expected+="IMM-ul "+MGT.toString()+" a achitat serviciul "+serviciu.Consultanta.toString()+sep;
+		assertEquals(expected, outContent.toString());
 	}
 
 	@After
